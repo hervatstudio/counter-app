1f1047526ddc892588e3dad20dcef55d051ff519
Alamin Mir <hervatstudio@gmail.com>
2019-03-16 16:16:20 +0600

    16 Mar 2019

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,18 +6,39 @@ class Counter extends Component {
         tags: []
     };
 
-    renderTags(){
-        if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-        return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>;
-    }
+    
+
+    // renderTags(){
+    //     if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    //     return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>;
+    // }
     render() { 
         return(
+            
             <div>
-                {this.state.tags.length === 0 && 'Please create a new tag!'} 
-                {this.renderTags()}
+                {/* {this.state.tags.length === 0 && 'Please create a new tag!'} 
+                {this.renderTags()} */}
+
+                <span className={this.getBadgeClasses()}>{this.state.count}</span>
+                <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
     }
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+    
+    handleIncrement = () => {
+        this.setState({
+            count: this.state.count + 1
+        })
+        
+    }
+
+   
 }
  
 export default Counter;
